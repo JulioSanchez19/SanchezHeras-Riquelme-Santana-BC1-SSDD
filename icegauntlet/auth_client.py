05d6520b966e3a4df2acfbf38deecac6b8a482e7
AlejandroRiquelme1 <72739347+AlejandroRiquelme1@users.noreply.github.com>
2020-12-04 00:37:13 +0100

    Version 1.1.0
    Cliente de Juego funcional

--- icegauntlet/auth_client.py
+++ icegauntlet/auth_client.py
@@ -7,6 +7,7 @@ import atexit
 import logging
 import argparse
 import hashlib
+import getpass
 
 import Ice
 Ice.loadSlice('icegauntlet.ice')
@@ -14,15 +15,15 @@ import IceGauntlet
 
 class clienteAutenticacion(Ice.Application):
     def run(self, argv):
-        proxy = self.communicator().stringToProxy(argv[2])
+        proxy = self.communicator().stringToProxy(argv[3])
         auth = IceGauntlet.AuthenticationPrx.checkedCast(proxy)
 
         if not auth:
             raise RuntimeError('Invalid proxy')
         print(argv[1])
-        if argv[1]=="1":
+        if argv[1]=="-t":
             self.conseguirToken(argv, auth)
-        elif argv[1]=="2":
+        elif argv[1]=="-c":
             self.cambiarPassword(argv, auth)
         else:
             print("Que quieres hacer")
@@ -33,10 +34,11 @@ class clienteAutenticacion(Ice.Application):
     def calcularHash(self,password):
         return hashlib.sha256(password.encode()).hexdigest()
     
-    def conseguirToken(self,argv, auth):
-        passwordHash=self.calcularHash(argv[4])
+    def conseguirToken(self,argv, auth):#python3 auth_client.py -t proxy usuario 
+        password=getpass.getpass(prompt="Enter password:")
+        passwordHash=self.calcularHash(password)
         print(passwordHash)
-        token=auth.getNewToken(argv[3], passwordHash)
+        token=auth.getNewToken(argv[2], passwordHash)
         print(token)
     
     def cambiarPassword(self, argv,auth):
