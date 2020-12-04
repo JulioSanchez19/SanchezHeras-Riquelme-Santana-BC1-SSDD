2a494e1ecb15edd8472776ddd2c80b3799827548
JavierSantanaDelgado <72739114+JavierSantanaDelgado@users.noreply.github.com>
2020-12-04 11:16:17 +0100

    Version 1.3.0
    Auth_client completado, el archivo token.txt guarda el token.

--- icegauntlet/auth_client.py
+++ icegauntlet/auth_client.py
@@ -3,6 +3,7 @@
 # pylint: disable=W1203
 
 import sys
+import os
 import atexit
 import logging
 import argparse
@@ -15,35 +16,59 @@ import IceGauntlet
 
 class clienteAutenticacion(Ice.Application):
     def run(self, argv):
+        
+        if len(argv)!=4:
+            print("usage: auth_client.py <-g/-p> <user> <proxy> ")
+
         proxy = self.communicator().stringToProxy(argv[3])
         auth = IceGauntlet.AuthenticationPrx.checkedCast(proxy)
 
         if not auth:
             raise RuntimeError('Invalid proxy')
-        print(argv[1])
-        if argv[1]=="-t":
-            self.conseguirToken(argv, auth)
-        elif argv[1]=="-c":
-            self.cambiarPassword(argv, auth)
-        else:
-            print("Que quieres hacer")
-        
 
+        self.user=argv[2]
+
+        if argv[1]=="-g":
+            self.conseguirToken(self.user, auth)
+        elif argv[1]=="-c":
+            self.cambiarPassword(self.user, auth)
+        else:
+            print("Opciones -g -c")
+        
         return 0
 
     def calcularHash(self,password):
         return hashlib.sha256(password.encode()).hexdigest()
     
-    def conseguirToken(self,argv, auth):#python3 auth_client.py -t proxy usuario 
+    def conseguirToken(self,user, auth):#python3 auth_client.py -t proxy usuario
+        token=""
         password=getpass.getpass(prompt="Enter password:")
         passwordHash=self.calcularHash(password)
-        print(passwordHash)
-        token=auth.getNewToken(argv[2], passwordHash)
-        print(token)
+        try:
+            token=auth.getNewToken(user, passwordHash)
+        except IceGauntlet.Unauthorized:
+            print("Usuario y/o contraseña no valida")
+            os._exit(-1)
+        with open("token.txt", 'w') as tokenTxt:#se escribe el nuevo token en el archivo token.txt para futuras operaciones
+            tokenTxt.write(token)
     
-    def cambiarPassword(self, argv,auth):
-        passwordHash=self.calcularHash(argv[4])
-        newPasswordHash=self.calcularHash(argv[5])
-        auth.changePassword(argv[3],passwordHash, newPasswordHash)
-
+    def cambiarPassword(self, user,auth):
+        currentPassword=getpass.getpass(prompt="Enter current password:")
+        if(currentPassword == ""):
+            newPasswordHash=self.calcularHash(getpass.getpass(prompt="Enter new password:"))
+            try:
+                auth.changePassword(user,None, newPasswordHash)
+            except IceGauntlet.Unauthorized:
+                print("Usuario y/o contraseña no valida")
+                os._exit(-1)
+            
+        else:
+            currentPasswordHash=self.calcularHash(currentPassword)
+            newPasswordHash=self.calcularHash(getpass.getpass(prompt="Enter new password:"))
+            try:
+                auth.changePassword(user,currentPasswordHash, newPasswordHash)
+            except IceGauntlet.Unauthorized:
+                print("Usuario y/o contraseña no valida")
+                os._exit(-1)
+            
 sys.exit(clienteAutenticacion().main(sys.argv))
